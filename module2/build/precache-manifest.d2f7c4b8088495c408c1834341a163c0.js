c7bcad9c2932d102e46ce2accde2572a1b5e317b
Oleh Zharovskyi <olegzhora11@gmail.com>
2019-08-29 14:00:26 +0300

    add

---
+++ module2/build/precache-manifest.d2f7c4b8088495c408c1834341a163c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d3511878c47d055e71ac843e27e17a3",
+    "url": "/ReactHome/module2/build/index.html"
+  },
+  {
+    "revision": "fdc71f7a805a227f6153",
+    "url": "/ReactHome/module2/build/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "3f9fc989b79de10a33d1",
+    "url": "/ReactHome/module2/build/static/js/2.64ed0093.chunk.js"
+  },
+  {
+    "revision": "fdc71f7a805a227f6153",
+    "url": "/ReactHome/module2/build/static/js/main.5a646be0.chunk.js"
+  },
+  {
+    "revision": "35dc489cc65c465729ab",
+    "url": "/ReactHome/module2/build/static/js/runtime~main.922ce598.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactHome/module2/build/static/media/logo.5d5d9eef.svg"
+  }
+]);
