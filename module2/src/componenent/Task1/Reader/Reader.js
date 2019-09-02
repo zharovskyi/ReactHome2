5de16b10942cc864e30ed0e2e6a1ccafdd20b6f7
Oleh Zharovskyi <olegzhora11@gmail.com>
2019-09-02 15:59:06 +0300

    add

---
+++ module2/src/componenent/Task1/Reader/Reader.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class Reader extends Component {
+    // static defaultProps = {};
+
+    // static propTypes = {};
+    render() {
+        return (
+            <h1>Pupkin</h1>
+        )
+    }
+}
+
+export default Reader;
